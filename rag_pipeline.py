56ad7b0092e1f73296c4f5fa397107142fa4f2e2
sahana <sahanaknkoc@gmail.com>
2026-03-28 11:53:32 +0530

    fix langchain prompts import path

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -11,7 +11,7 @@ import os
 from typing import List, Tuple
 from langchain_core.documents import Document
 from langchain_groq import ChatGroq
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 
